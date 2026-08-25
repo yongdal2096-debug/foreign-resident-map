89ab3acd1953082f55e4f58c42ab66ce2268ad61
yongdal2096-debug <yongdal2096@gmail.com>
2026-08-25 21:35:59 +0900

    refactor: split public resident dataset for Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import ResidentDashboard, { type ResidentData } from "./ResidentDashboard";
-import residents from "./data/residents.json";
+import ResidentDashboard, { type Province, type ResidentData } from "./ResidentDashboard";
+import summary from "./data/summary.json";
+import provinces1 from "./data/provinces-1.json";
+import provinces2 from "./data/provinces-2.json";
+import provinces3 from "./data/provinces-3.json";
+import provinces4 from "./data/provinces-4.json";
+import provinces5 from "./data/provinces-5.json";
+
+const residents: ResidentData = {
+  ...summary,
+  provinces: [
+    ...provinces1,
+    ...provinces2,
+    ...provinces3,
+    ...provinces4,
+    ...provinces5,
+  ] as Province[],
+};
 
 export default function Home() {
   return (
     <ResidentDashboard
-      data={residents as ResidentData}
+      data={residents}
       naverClientId={process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID ?? ""}
     />
   );
